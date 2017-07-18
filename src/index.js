ba842b874d510ec31aac9eecdaa6a21a6835b957
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-07-18 16:59:27 +0300

    routing implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
-import Routes from './routes/index.js'
+import Routes from './routes'
 
 ReactDOM.render(
   <Router
